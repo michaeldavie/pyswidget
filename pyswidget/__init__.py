6dcbafe768a09f6cc6309f42a28fcdc445da25a8
build <michael.k.kehoe@gmail.com>
2022-05-14 18:44:18 -0700

    Fixing imports

---
+++ pyswidget/__init__.py
@@ -0,0 +1,20 @@
+from importlib_metadata import version  # type: ignore
+
+from pyswidget.discovery import SwidgetProtocol
+from pyswidget.swidgetdimmer import SwidgetDimmer
+from pyswidget.device import DeviceType, SwidgetDevice
+from pyswidget.swidgetoutlet import SwidgetOutlet
+from pyswidget.swidgetswitch import SwidgetSwitch
+
+__version__ = version("pyswidget")
+
+
+__all__ = [
+    "SwidgetProtocol",
+    "DeviceType",
+    "SwidgetDevice",
+    "SwidgetException",
+    "SwidgetDimmer",
+    "SwidgetOutlet",
+    "SwidgetSwitch",
+]

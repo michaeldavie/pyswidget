07e91c33696a26f5f3cd7a25e36c83226e036ab6
Michael Davie <michael.davie@gmail.com>
2022-05-21 07:36:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
-setup(name='pyswidget',
-      version='0.0.7',
-      description='Python Swidget Library',
-      author='Michael Kehoe',
-      author_email='redacted',
-      url='https://www.python.org',
-      packages=['pyswidget'],
-     )
+setup(
+    name="pyswidget",
+    version="0.0.7",
+    description="Python Swidget Library",
+    author="Michael Kehoe",
+    author_email="redacted@email.nope",
+    url="https://www.python.org",
+    packages=["pyswidget"],
+)
